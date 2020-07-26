91efa4f3b576519462e63e4492574e3527f3a8b1
Wooooohh <1020227487@qq.com>
2020-07-26 11:54:41 +0800

    [parkingsystem] [Jack] feature: pass should_return_ticket_with_lot2_when_park_given_super_smart_parking_boy_and_car_and_two_parking_lot_and_lot2_rate_is_large

--- src/main/java/com/oocl/parkingsystem/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/parkingsystem/SuperSmartParkingBoy.java
@@ -9,4 +9,29 @@ public class SuperSmartParkingBoy extends ParkingBoy{
         super(parkingLots);
     }
 
+    public ParkingTicket park(Car car) {
+        int maxRateLotIndex = 0;
+        int currLotRate = 0;
+        if (carsMap.size() < totalCapacity) {
+            for(int i = 0; i < parkingLots.size(); i++){
+                ParkingLot parkingLot = parkingLots.get(i);
+                if (!parkingLots.get(i).isFull()) {
+                    int rate = (parkingLot.getCapacity() - parkingLot.getCarsNum()) / parkingLot.getCapacity();
+                    if( rate > currLotRate){
+                        maxRateLotIndex = i;
+                        currLotRate = rate;
+                    }
+                }
+            }
+            String carId = car.getCarId();
+            ParkingLot maxEmptyPositionLot = parkingLots.get(maxRateLotIndex);
+            maxEmptyPositionLot.putCar(car);
+            ParkingTicket parkingTicket = new ParkingTicket(carId, maxEmptyPositionLot.getLotId(), maxEmptyPositionLot.getPosition());
+            String ticketId = parkingTicket.getTicketId();
+            carsMap.put(ticketId, carId);
+            return parkingTicket;
+        }
+        return null;
+    }
+
 }
